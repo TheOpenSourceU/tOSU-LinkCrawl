27cdb49cf34290a23ebdbe50a5397c3a974c5d8a
Frank Villasenor <f.villasenor@arkadin.com>
2018-03-02 14:53:25 -0600

    Interesting bug where remove change element to null.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const scrapeIt = require("scrape-it");
 const HashSet = require('hashset');
 
 const uniqueInternalLinks = new HashSet();
+const completed = new HashSet();
 
 const filters = {
     homePage: {
@@ -34,28 +35,58 @@ const filters = {
 };
 
 function report({data, response}) {
-    console.log(`Status Code: ${response.statusCode}`)
-    console.log(JSON.stringify(data, null, ' '));
+    console.log(`Status Code: ${response.statusCode}`);
+    if(response.statusCode !== 200) throw new "statusCode not 200";
+    //console.log(JSON.stringify(data, null, ' '));
     return data;
 }
 function addToLinks(data) {
     //go through data and push to uniqueInternalLinks
     data.articleLinks.forEach((itm) => {
         //uniqueInternalLinks.push(itm.link)
-        uniqueInternalLinks.add(itm.link);
+        if(itm.link) uniqueInternalLinks.add(itm.link);
     });
 
     if(data.sideMenu) {
         data.sideMenu.forEach((itm) => {
             //uniqueInternalLinks.push(itm.menulink)
-            uniqueInternalLinks.add(itm.menulink);
+            if(itm.menulink && itm.menulink.length > 1 && itm.menulink !== "/rss/") uniqueInternalLinks.add(itm.menulink);
         });
     }
     return true; //uniqueInternalLinks is global
 }
 
+function printHashset() {
+    console.log( JSON.stringify(uniqueInternalLinks.toArray(), null, 2) );
+}
+
+// From here on out, it's a loop.
+function nextPage() {
+    //take one from the list.
+    if(uniqueInternalLinks.length) {
+        const link = uniqueInternalLinks.toArray()[0];
+        console.log("-------------------------------");
+        console.log("Page", link);
+        if(link) {
+            scrapeIt(`https://theOpenSourceU.org${link}`, filters.page)
+                .then(report)
+                .then((d) => {
+                    completed.add(link);
+                    uniqueInternalLinks.remove(link);
+                })
+                .then(nextPage);
+        } else {
+            printHashset();
+            nextPage();
+        }
+    }
+}
+
+
 // Promise interface
 scrapeIt("https://theOpenSourceU.org", filters.homePage)
     .then(report)
     .then(addToLinks)
-    .then(nextPage);
+    .then(printHashset)
+    .then(nextPage)
+;
