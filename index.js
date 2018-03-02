456c5c56217bef9a1c26c75a7167483388790d1a
Frank Villasenor <f.villasenor@arkadin.com>
2018-03-02 14:39:02 -0600

    Ideas...

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 
 const scrapeIt = require("scrape-it");
+const HashSet = require('hashset');
+
+const uniqueInternalLinks = new HashSet();
 
 const filters = {
     homePage: {
@@ -8,7 +11,7 @@ const filters = {
         articleLinks: {
             listItem: "article",
             data: {
-                links: {
+                link: {
                     selector: "a",
                     attr: "href"
                 }
@@ -17,7 +20,7 @@ const filters = {
         sideMenu: {
             listItem: ".nav ul li",
             data: {
-                menulinks: {
+                menulink: {
                     selector: "a",
                     attr: "href",
                     convert: function(d) {
@@ -30,9 +33,29 @@ const filters = {
     }
 };
 
-// Promise interface
-scrapeIt("https://theOpenSourceU.org", filters.homePage)
-    .then(({ data, response }) => {
+function report({data, response}) {
     console.log(`Status Code: ${response.statusCode}`)
     console.log(JSON.stringify(data, null, ' '));
-});
+    return data;
+}
+function addToLinks(data) {
+    //go through data and push to uniqueInternalLinks
+    data.articleLinks.forEach((itm) => {
+        //uniqueInternalLinks.push(itm.link)
+        uniqueInternalLinks.add(itm.link);
+    });
+
+    if(data.sideMenu) {
+        data.sideMenu.forEach((itm) => {
+            //uniqueInternalLinks.push(itm.menulink)
+            uniqueInternalLinks.add(itm.menulink);
+        });
+    }
+    return true; //uniqueInternalLinks is global
+}
+
+// Promise interface
+scrapeIt("https://theOpenSourceU.org", filters.homePage)
+    .then(report)
+    .then(addToLinks)
+    .then(nextPage);
